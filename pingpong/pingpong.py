190a1be8732de3f3027697feea630645dc4543da
FixDemolisher <fixigrok@gmail.com>
2026-04-19 16:23:04 +0300

    ping_pongV1
    Добавлены ракетки с управлением на стрелки и WASD

--- pingpong/pingpong.py
+++ pingpong/pingpong.py
@@ -1,9 +1,9 @@
 from pygame import *
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.image = transform.scale(image.load(player_image), (width, height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -13,12 +13,19 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if (keys[K_LEFT] or keys[K_a])and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if (keys[K_RIGHT] or keys[K_d]) and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if (keys[K_UP])and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 155:
+            self.rect.y += self.speed
+    
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 155:
+            self.rect.y += self.speed
 
 
 back = (200, 255, 255)
@@ -31,6 +38,11 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -38,6 +50,11 @@ while game:
 
     if not(finish):
         window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
     
     display.update()
     clock.tick(FPS)
